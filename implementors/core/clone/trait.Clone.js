17e3cd1a3c276d386fc6df9fea034c282ed75327
adjivas <adjivas@users.noreply.github.com>
2016-06-02 01:22:19 +0200

    add license to official page

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['sig'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='sig/ffi/enum.Sig.html' title='sig::ffi::Sig'>Sig</a>",];
+implementors["sig"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='sig/ffi/enum.Sig.html' title='sig::ffi::Sig'>Sig</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['sig'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='sig/ffi/enum.Sig.html' title='sig::ffi::Sig'>Sig</a>",];
+implementors["sig"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='sig/ffi/enum.Sig.html' title='sig::ffi::Sig'>Sig</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
